2eae4e1bf3bdd03366607c9794666d3c67315860
Koushik <koushiknus@gmail.com>
2023-12-23 22:52:54 -0600

    Update : Row Column Sample updated

--- SampleJetPackComposeApp/app/src/main/java/com/example/samplejetpackcomposeapp/RowColumnDemoActivity.kt
+++ SampleJetPackComposeApp/app/src/main/java/com/example/samplejetpackcomposeapp/RowColumnDemoActivity.kt
@@ -4,16 +4,14 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.samplejetpackcomposeapp.ui.theme.SampleJetPackComposeAppTheme
 
 class RowColumnDemoActivity : ComponentActivity() {
@@ -25,13 +23,26 @@ class RowColumnDemoActivity : ComponentActivity() {
     }
 
     @Composable
-    fun MainScreen(){
-        Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
-            Row( verticalAlignment = Alignment.CenterVertically){
-                Button(onClick = {Log.i("Button1","Clicked")}) {
+    fun MainScreen() {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly) {
+                Button(onClick = {
+                    Log.i("Button1", "Clicked")
+                }, modifier = Modifier.padding(5.dp)) {
                     Text(text = "Click me 3")
                 }
-                Button(onClick = { Log.i("Button2","Clicked")}) {
+                Text(text = "0")
+                Button(
+                    onClick = { Log.i("Button2", "Clicked") },
+                    modifier = Modifier.padding(5.dp)
+                ) {
                     Text(text = "Click me 2")
                 }
             }
@@ -41,7 +52,7 @@ class RowColumnDemoActivity : ComponentActivity() {
     @Preview(showBackground = true)
     @Composable
     fun DefaultPreview() {
-        SampleJetPackComposeAppTheme {
+        setContent {
             MainScreen()
         }
     }
