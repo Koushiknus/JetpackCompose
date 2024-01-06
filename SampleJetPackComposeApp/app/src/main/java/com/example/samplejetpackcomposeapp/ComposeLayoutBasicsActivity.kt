6f86885bf8de7da3c99bafc711612d7c877ef0eb
Koushik <koushiknus@gmail.com>
2024-01-06 14:55:20 -0600

    Update : Compose State Added

--- SampleJetPackComposeApp/app/src/main/java/com/example/samplejetpackcomposeapp/ComposeLayoutBasicsActivity.kt
+++ SampleJetPackComposeApp/app/src/main/java/com/example/samplejetpackcomposeapp/ComposeLayoutBasicsActivity.kt
@@ -1,29 +1,29 @@
 package com.example.samplejetpackcomposeapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -34,7 +34,7 @@ class ComposeLayoutBasicsActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            UserList()
+            MainContent()
         }
     }
 
@@ -49,7 +49,26 @@ class ComposeLayoutBasicsActivity : ComponentActivity() {
     )
 
     @Composable
-    fun UserList(){
+    fun MainContent(){
+        val user = User(1)
+        val users = remember {
+            mutableStateListOf(user)
+        }
+        Box(modifier = Modifier.fillMaxSize()) {
+            UserList(users)
+            Button(
+                modifier = Modifier
+                    .padding(24.dp)
+                    .align(Alignment.BottomCenter),
+                onClick = {
+                    users.add(User(1))
+                }) {
+                Text(text = "Add More")
+            }
+        }
+    }
+    @Composable
+    fun UserList(users: MutableList<User>) {
      /*   Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
             for(i in 1..10){
                 UserCard()
@@ -106,7 +125,7 @@ class ComposeLayoutBasicsActivity : ComponentActivity() {
     @Composable
     fun DefaultPreview() {
         setContent {
-            UserCard()
+            MainContent()
         }
     }
 }
